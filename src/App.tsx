e126fbeb2dd4d82c41f1f4f2dfeec2fba113399a
Daniel Suen <100717879+dan-suen@users.noreply.github.com>
2026-03-06 22:30:52 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import type { CSSProperties } from "react";
 import { createClient } from "@supabase/supabase-js";
 
